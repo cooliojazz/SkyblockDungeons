bf3c45cf7e1f6770902378d0d819ee53f29e703f
Ricky Talbot <rickytalbot@hotmail.com>
2016-05-08 15:24:13 -0600

    Adding scripting options

---
+++ src/com/up/sd/Action.java
@@ -0,0 +1,18 @@
+package com.up.sd;
+
+/**
+ *
+ * @author ricky.t
+ */
+class Action {
+    
+    enum ACTIONTYPE {
+        TELEPORT,
+        
+    }
+
+    void run() {
+        
+    }
+    
+}
---
+++ src/com/up/sd/Condition.java
@@ -0,0 +1,19 @@
+package com.up.sd;
+
+import org.bukkit.entity.Player;
+
+/**
+ *
+ * @author ricky.t
+ */
+class Condition {
+    
+    enum CONDITIONYTPE {
+        AREA,
+        
+    }
+    
+    public boolean passes(Player p) {
+        
+    }
+}
